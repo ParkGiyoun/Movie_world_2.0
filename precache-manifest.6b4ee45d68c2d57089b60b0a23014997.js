1fbd54f87b8ac9d64824172c5000ffe076e31b23
cosil2b2 <53854070+cosil2b2@users.noreply.github.com>
2020-08-04 00:30:56 +0900

    Updates

---
+++ precache-manifest.6b4ee45d68c2d57089b60b0a23014997.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74eaa2df4d043cb17a107bb4c8290882",
+    "url": "/Movie_world_2.0/index.html"
+  },
+  {
+    "revision": "03fac90273a6c794bf85",
+    "url": "/Movie_world_2.0/static/css/main.e65f3f68.chunk.css"
+  },
+  {
+    "revision": "bbe2b017f80b15dae4d9",
+    "url": "/Movie_world_2.0/static/js/2.e14eeeec.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Movie_world_2.0/static/js/2.e14eeeec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03fac90273a6c794bf85",
+    "url": "/Movie_world_2.0/static/js/main.a045eab6.chunk.js"
+  },
+  {
+    "revision": "81dfedd48c011e34bef3",
+    "url": "/Movie_world_2.0/static/js/runtime-main.28ff84c5.js"
+  }
+]);
